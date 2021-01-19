34cad265e9a70f24486a894479abab15d87c69a1
SaadAbt <45111225+SaadAbt@users.noreply.github.com>
2021-01-19 01:18:35 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,7 +38,7 @@ const About: React.FC = () => (
     <Helmet>
       <title>About</title>
       <script src="termynal.js"></script>
-      <link rel="stylesheet" type="text/css" href="https://thibaud-robin.fr/css/termynal.css">
+      
     </Helmet>
     <Wrapper css={PageTemplate}>
       <header className="site-archive-header no-image" css={[SiteHeader, SiteArchiveHeader]}>
@@ -63,36 +63,9 @@ const About: React.FC = () => (
                 
                 
                 <p>
-    <Helmet>
-    <div id="termynal">
-        <span data-ty="input" data-ty-prompt="[th1b4ud@th1b4ud-pc ~]$" data-ty-cursor="▋">python3</span>
-        <span data-ty data-ty-delay="700">Python 3.8.1 - [GCC 8.1.1] on linux - Type "help", "copyright", "credits" or "license" for more information</span>
-        <span data-ty="input" data-ty-delay="300"  data-ty-prompt=">>>" data-ty-cursor="▋">import platform</span>
-        <span data-ty="input" data-ty-delay="300"  data-ty-prompt=">>>" data-ty-cursor="▋">os = platform.system()</span>
-        <span data-ty="input" data-ty-prompt=">>>" data-ty-cursor="▋">def whoami():</span>
-        <span data-ty="input" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if os == "Windows":</span>
-        <span data-ty="input"  data-ty-delay="700" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("Cool guy ! Windows it's a usefull tool in internal penetration test :)")</span>
-        <span data-ty="input" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elif os == "Linux":</span>
-        <span data-ty="input" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if "ARCH" in platform.uname()[2]:</span>
-        <span data-ty="input" data-ty-delay="700" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("Haha the best ! Yeah ArchLinux is the best distrib :D")</span>
-        <span data-ty="input" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;else:</span>
-        <span data-ty="input" data-ty-prompt="..." data-ty-cursor="▋">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("Nice guy ! But why don't have a better distrib ? :)")</span>
-        <span data-ty="input" data-ty-delay="1000" data-ty-prompt="..." data-ty-cursor="▋"></span>
-        <span data-ty="input" data-ty-prompt=">>>" data-ty-cursor="▋">whoami()</span>
-        <span data-ty data-ty-delay="100">Haha the best ! Yeah ArchLinux is the best distrib :D</span>
-    </div>
-
-    <script>
-    var termynal = new Termynal('#termynal', { 
-        startDelay: 600,
-        typeDelay: 50,
-        lineDelay: 150
-    });
-    </script>
-    </Helmet>
                 
                 
-                
+              
                 <p>
                   
                 </p>
